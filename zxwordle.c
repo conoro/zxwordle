5b435e3d7fe1e5c9b0e0b0419688d51c1cd3b61b
Conor ONeill <conor@conoroneill.com>
2022-03-26 09:47:06 +0000

    first commit

---
+++ zxwordle.c
@@ -0,0 +1,129 @@
+/*
+  Compile with:
+
+  zcc +zx -vn -startup=1 -clib=sdcc_iy zxwordle.c -o zxwordle -create-app
+
+*/
+
+#include <arch/zx.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    static const char allthewords[739][6] = {"saute", "allow", "renew", "their", "slosh", "purge", "chest", "depot", "epoxy", "nymph", "found", "shall", "harry", "stove", "lowly", "snout", "trope", "fewer", "shawl", "natal", "comma", "foray", "scare", "stair", "black", "squad", "royal", "chunk", "mince", "shame", "cheek", "ample", "flair", "foyer", "cargo", "oxide", "plant", "olive", "inert", "askew", "heist", "shown", "zesty", "hasty", "trash", "fella", "larva", "forgo", "story", "hairy", "train", "homer", "badge", "midst", "canny", "fetus", "butch", "farce", "slung", "tipsy", "metal", "yield", "delve", "being", "scour", "glass", "gamer", "scrap", "money", "hinge", "album", "vouch", "asset", "tiara", "crept", "bayou", "atoll", "manor", "creak", "showy", "phase", "froth", "depth", "gloom", "flood", "trait", "girth", "piety", "payer", "goose", "float", "donor", "atone", "primo", "apron", "blown", "cacao", "loser", "input", "gloat", "awful", "brink", "smite", "beady", "rusty", "retro", "droll", "gawky", "hutch", "pinto", "gaily", "egret", "lilac", "sever", "field", "fluff", "hydro", "flack", "agape", "voice", "stead", "stalk", "berth", "madam", "night", "bland", "liver", "wedge", "augur", "roomy", "wacky", "flock", "angry", "bobby", "trite", "aphid", "tryst", "midge", "power", "elope", "cinch", "motto", "stomp", "upset", "bluff", "cramp", "quart", "coyly", "youth", "rhyme", "buggy", "alien", "smear", "unfit", "patty", "cling", "glean", "label", "hunky", "khaki", "poker", "gruel", "twice", "twang", "shrug", "treat", "unlit", "waste", "merit", "woven", "octal", "needy", "clown", "widow", "irony", "ruder", "gauze", "chief", "onset", "prize", "fungi", "charm", "gully", "inter", "whoop", "taunt", "leery", "class", "theme", "lofty", "tibia", "booze", "alpha", "thyme", "eclat", "doubt", "parer", "chute", "stick", "trice", "alike", "sooth", "recap", "saint", "liege", "glory", "grate", "admit", "brisk", "soggy", "usurp", "scald", "scorn", "leave", "twine", "sting", "bough", "marsh", "sloth", "dandy", "vigor", "howdy", "enjoy", "valid", "ionic", "equal", "unset", "floor", "catch", "spade", "stein", "exist", "quirk", "denim", "grove", "spiel", "mummy", "fault", "foggy", "flout", "carry", "sneak", "libel", "waltz", "aptly", "piney", "inept", "aloud", "photo", "dream", "stale", "vomit", "ombre", "fanny", "unite", "snarl", "baker", "there", "glyph", "pooch", "hippy", "spell", "folly", "louse", "gulch", "vault", "godly", "threw", "fleet", "grave", "inane", "shock", "crave", "spite", "valve", "skimp", "claim", "rainy", "musty", "pique", "daddy", "quasi", "arise", "aging", "valet", "opium", "avert", "stuck", "recut", "mulch", "genre", "plume", "rifle", "count", "incur", "total", "wrest", "mocha", "deter", "study", "lover", "safer", "rivet", "funny", "smoke", "mound", "undue", "sedan", "pagan", "swine", "guile", "gusty", "equip", "tough", "canoe", "chaos", "covet", "human", "udder", "lunch", "blast", "stray", "manga", "melee", "lefty", "quick", "paste", "given", "octet", "risen", "groan", "leaky", "grind", "carve", "loose", "sadly", "spilt", "apple", "slack", "honey", "final", "sheen", "eerie", "minty", "slick", "derby", "wharf", "spelt", "coach", "erupt", "singe", "price", "spawn", "fairy", "jiffy", "filmy", "stack", "chose", "sleep", "ardor", "nanny", "niece", "woozy", "handy", "grace", "ditto", "stank", "cream", "usual", "diode", "valor", "angle", "ninja", "muddy", "chase", "reply", "prone", "spoil", "heart", "shade", "diner", "arson", "onion", "sleet", "dowel", "couch", "palsy", "bowel", "smile", "evoke", "creek", "lance", "eagle", "idiot", "siren", "built", "embed", "award", "dross", "annul", "goody", "frown", "patio", "laden", "humid", "elite", "lymph", "edify", "might", "reset", "visit", "gusto", "purse", "vapor", "crock", "write", "sunny", "loath", "chaff", "slide", "queer", "venom", "stamp", "sorry", "still", "acorn", "aping", "pushy", "tamer", "hater", "mania", "awoke", "brawn", "swift", "exile", "birch", "lucky", "freer", "risky", "ghost", "plier", "lunar", "winch", "snare", "nurse", "house", "borax", "nicer", "lurch", "exalt", "about", "savvy", "toxin", "tunic", "pried", "inlay", "chump", "lanky", "cress", "eater", "elude", "cycle", "kitty", "boule", "moron", "tenet", "place", "lobby", "plush", "vigil", "index", "blink", "clung", "qualm", "croup", "clink", "juicy", "stage", "decay", "nerve", "flier", "shaft", "crook", "clean", "china", "ridge", "vowel", "gnome", "snuck", "icing", "spiny", "rigor", "snail", "flown", "rabid", "prose", "thank", "poppy", "budge", "fiber", "moldy", "dowdy", "kneel", "track", "caddy", "quell", "dumpy", "paler", "swore", "rebar", "scuba", "splat", "flyer", "horny", "mason", "doing", "ozone", "amply", "molar", "ovary", "beset", "queue", "cliff", "magic", "truce", "sport", "fritz", "edict", "twirl", "verse", "llama", "eaten", "range", "whisk", "hovel", "rehab", "macaw", "sigma", "spout", "verve", "sushi", "dying", "fetid", "brain", "buddy", "thump", "scion", "candy", "chord", "basin", "march", "crowd", "arbor", "gayly", "musky", "stain", "dally", "bless", "bravo", "stung", "title", "ruler", "kiosk", "blond", "ennui", "layer", "fluid", "tatty", "score", "cutie", "zebra", "barge", "matey", "bluer", "aider", "shook", "river", "privy", "betel", "frisk", "bongo", "begun", "azure", "weave", "genie", "sound", "glove", "braid", "scope", "wryly", "rover", "assay", "ocean", "bloom", "irate", "later", "woken", "silky", "wreck", "dwelt", "slate", "smack", "solid", "amaze", "hazel", "wrist", "jolly", "globe", "flint", "rouse", "civil", "vista", "relax", "cover", "alive", "beech", "jetty", "bliss", "vocal", "often", "dolly", "eight", "joker", "since", "event", "ensue", "shunt", "diver", "poser", "worst", "sweep", "alley", "creed", "anime", "leafy", "bosom", "dunce", "stare", "pudgy", "waive", "choir", "stood", "spoke", "outgo", "delay", "bilge", "ideal", "clasp", "seize", "hotly", "laugh", "sieve", "block", "meant", "grape", "noose", "hardy", "shied", "drawl", "daisy", "putty", "strut", "burnt", "tulip", "crick", "idyll", "vixen", "furor", "geeky", "cough", "naive", "shoal", "stork", "bathe", "aunty", "check", "prime", "brass", "outer", "furry", "razor", "elect", "evict", "imply", "demur", "quota", "haven", "cavil", "swear", "crump", "dough", "gavel", "wagon", "salon", "nudge", "harem", "pitch", "sworn", "pupil", "excel", "stony", "cabin", "unzip", "queen", "trout", "polyp", "earth", "storm", "until", "taper", "enter", "child", "adopt", "minor", "fatty", "husky", "brave", "filet", "slime", "glint", "tread", "steal", "regal", "guest", "every", "murky", "share", "spore", "hoist", "buxom", "inner", "otter", "dimly", "level", "sumac", "donut", "stilt", "arena", "sheet"};
+
+    unsigned char word[6] = "dunno";
+    unsigned char guess[6];
+    unsigned char matches[6] = "FFFFF";
+    unsigned char blocks[6] = "BBBBB";
+    int tries;
+    int wordlenum = 0;
+    int first_wordle = 272;
+    int valid_wordle;
+    int valid_guess = 0;
+    char *p, s[100];
+
+    valid_wordle = 0;
+    wordlenum = 0;
+
+    zx_cls(PAPER_WHITE);
+
+    printf("\x16\x01\x01Welcome to ZX Wordle\n");
+    printf("\x10\x30\x11\x37--------------------\n");
+    printf("\x10\x30\x11\x37Wordle number (272-1010): ");
+
+    while (fgets(s, sizeof(s), stdin))
+    {
+        wordlenum = strtol(s, &p, 10);
+        if (p == s || *p != '\n')
+        {
+            printf("\x10\x30\x11\x37Wordle number (272-1010): ");
+        }
+        else if ((wordlenum < 272) || (wordlenum > 1010))
+        {
+            printf("\x10\x30\x11\x37Wordle number (272-1010): ");
+        }
+        else
+            break;
+    }
+
+    strcpy(word, allthewords[wordlenum - first_wordle]);
+
+    for (tries = 0; tries < 6; tries++)
+    {
+        strcpy(blocks, "RRRRR");
+        strcpy(matches, "FFFFF");
+        valid_guess = 0;
+
+        while (valid_guess == 0)
+        {
+            printf("Try %d: ", tries + 1);
+            scanf("%s", &guess);
+            if (strlen(guess) == 5)
+            {
+                valid_guess = 1;
+            }
+            else
+            {
+                printf("Must be 5 letters, try again.\n");
+            }
+        }
+
+        int i, j;
+
+        for (i = 0; i < 5; i++)
+        {
+            for (j = 0; j < 5; j++)
+            {
+                if ((guess[i] == word[j]) && (i == j))
+                {
+                    blocks[i] = 'G';
+                    matches[j] = 'T';
+                }
+                else if ((guess[i] == word[j]) && (i != j) && (blocks[i] != 'G') && (matches[j] != 'T'))
+                {
+                    blocks[i] = 'Y';
+                    matches[j] = 'T';
+                }
+            }
+        }
+
+        // 0=black 1=blue 2=red 3=magenta 4=green 5=cyan 6=yellow 7=white
+
+        int k;
+        printf("%s\n", guess);
+        for (k = 0; k < 5; k++)
+        {
+            if (blocks[k] == 'G')
+            {
+                printf("\x10\x34\x14 ");
+            }
+            else if (blocks[k] == 'Y')
+            {
+                printf("\x10\x36\x14 ");
+            }
+            else
+            {
+                printf("\x10\x32\x14 ");
+            }
+        }
+
+        printf("\x10\x30\x11\x37\n");
+
+        if ((tries < 5) && (strcmp(word, guess) != 0))
+        {
+        }
+        else if ((tries == 5) && (strcmp(word, guess) != 0))
+        {
+            printf("Sorry, the word was: %s", word);
+        }
+        else if (strcmp(word, guess) == 0)
+        {
+            printf("Yes!! It was: %s", word);
+            break;
+        }
+    }
+    return 0;
+}
